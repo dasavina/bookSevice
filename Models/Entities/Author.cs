3650b255224665700617aebc08e4aa2cb3a8c3ea
Savina Daria <savina.dariia@chnu.edu.ua>
2024-10-08 12:24:44 +0300

    Add project files.

---
+++ Models/Entities/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Pseudonym { get; set; }
+        public string ShortBio { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+
+
+
+}
---
+++ Models/Entities/BookPublisher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class BookPublisher
+    {
+        public int BookId { get; set; } // Foreign Key to Book
+        public Book Book { get; set; }
+
+        public int PublisherId { get; set; } // Foreign Key to Publisher
+        public Publisher Publisher { get; set; }
+    }
+
+}
